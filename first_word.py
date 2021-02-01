ee9fa627ead6fbd014ec9aca6301cf77ab14fd89
Yael Rashlin <58134106+yaelRashlin@users.noreply.github.com>
2021-02-01 20:18:27 +0200

    Create first_word.py

---
+++ first_word.py
@@ -0,0 +1,18 @@
+def first_word(text: str) -> str:
+    import re
+    return re.findall("\W?([a-zA-Z0-9']+)[\s|\.|\,]?", text)[0]
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(first_word("Hello world"))
+    
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert first_word("Hello world") == "Hello"
+    assert first_word(" a word ") == "a"
+    assert first_word("don't touch it") == "don't"
+    assert first_word("greetings, friends") == "greetings"
+    assert first_word("... and so on ...") == "and"
+    assert first_word("hi") == "hi"
+    assert first_word("Hello.World") == "Hello"
+    print("Coding complete? Click 'Check' to earn cool rewards!")
