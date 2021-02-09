94f76488a0fcb36ff8059263d1eff2c5fe06c533
Yael Rashlin <58134106+yaelRashlin@users.noreply.github.com>
2021-02-09 14:17:47 +0200

    Update is_acceptable_password.py

--- ellementray/is_acceptable_password.py
+++ ellementray/is_acceptable_password.py
@@ -1,5 +1,10 @@
+# Taken from mission Acceptable Password V
+
+import re
 def is_acceptable_password(password: str) -> bool:
-    if len(password) > 6:
+    if ((len(password) > 6 and len(re.findall("\d", password)) > 0 and \
+        len(password) != len(re.findall("\d", password))) or len(password) > 9) and \
+        not any(wrd in password for wrd in ["password","PASSWORD"]):
         return True
     return False
 
@@ -10,6 +15,19 @@ if __name__ == '__main__':
 
     # These "asserts" are used for self-checking and not for an auto-testing
     assert is_acceptable_password('short') == False
+    assert is_acceptable_password('short54') == True
     assert is_acceptable_password('muchlonger') == True
     assert is_acceptable_password('ashort') == False
+    assert is_acceptable_password('muchlonger5') == True
+    assert is_acceptable_password('sh5') == False
+    assert is_acceptable_password('1234567') == False
+    assert is_acceptable_password('12345678910') == True
+    assert is_acceptable_password('password12345') == False
+    assert is_acceptable_password('PASSWORD12345') == False
+    assert is_acceptable_password('pass1234word') == True
+    assert is_acceptable_password('aaaaaa1') == False
+    assert is_acceptable_password('aaaaaabbbbb') == False
+    assert is_acceptable_password('aaaaaabb1') == True
+    assert is_acceptable_password('abc1') == False
+    assert is_acceptable_password('abbcc12') == True
     print("Coding complete? Click 'Check' to earn cool rewards!")
